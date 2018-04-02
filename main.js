16e840c8466650561eca20bf01b1d52502b31f4a
nagasax26 <smarts003@gmail.com>
2018-04-02 14:14:44 +0300

    a simple todo list using jQuery

---
+++ main.js
@@ -0,0 +1,35 @@
+
+$('[type="submit"]').click(function(){
+  var inputTextValue = $('#todo-list').val();
+  if(inputTextValue === ''){
+      alert('Please enter a some text...');
+      return;
+    }
+
+  var name = 'todo-list';
+
+  var newCheckbox = '<input data-checked="false" type="checkbox" name='+name+'>'+
+  '<span style="color:red;">'+inputTextValue+'</span><br>';
+  $('.list').append(newCheckbox);
+
+
+
+  $('.list').off(); //binding off the event
+
+  $('.list').on('click','input[type="checkbox"]',checkedOn);
+});
+
+
+// TODO: need to add event listiner when checked on checkbox
+function checkedOn(){
+
+ $(this).data().checked = !$(this).data().checked;
+
+  if($(this).data().checked){
+    $(this).next().css('text-decoration','line-through');
+    $(this).next().css('color','green');
+    $(this).attr('disabled',true);
+  }
+
+
+}
